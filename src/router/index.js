b18ace44e4e09eaa69c2341bf3de977e9d83b1f8
Nam <njh3421@naver.com>
2021-10-12 16:02:22 +0900

    correcting,add change title, Make Login/Register

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Blogs from "../views/Blogs.vue"
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,17 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title:'Home'
+    }
+  },
+  {
+    path: "/blogs",
+    name: "Blogs",
+    component: Blogs,
+    meta:{
+      title:'Blogs  '
+    }
   },
 ];
 
@@ -16,6 +28,13 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+//to.meta.title => 위에 선언한 meta안에 title 즉 Home,Blogs를 가르킴
+//Home으로 라우트하면 Home|FireBlog가 되고 Blogs로 라우팅을 하면 Blogs|FireBlog가됨
+router.beforeEach((to ,from ,next) => {
+  document.title = `${to.meta.title} | FireBlog`;
+  next();
 });
 
 export default router;
