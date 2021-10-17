1afcd781f628ad05bb277f511bf573c41ebb7bf0
Nam <njh3421@naver.com>
2021-10-17 17:50:18 +0900

    Add Login,Register,Forgotpassword, Adding modal

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Blogs from "../views/Blogs.vue"
+import Login from "../views/Login.vue"
+import Register from "../views/Register.vue"
+import ForgotPassword from "../views/ForgotPassword.vue"
 
 Vue.use(VueRouter);
 
@@ -19,7 +22,31 @@ const routes = [
     name: "Blogs",
     component: Blogs,
     meta:{
-      title:'Blogs  '
+      title:'Blogs'
+    }
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta:{
+      title:'Login'
+    }
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta:{
+      title:'Register'
+    }
+  },
+  {
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    meta:{
+      title:'ForgotPassword'
     }
   },
 ];
---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset Password</h2>
+        <p>Forgot your password? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email">
+            <email class="icon"></email>
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import email from "../assets/Icons/envelope-regular.svg"
+
+export default {
+  name:"ForgotPassword",
+  components:{
+    email,
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.reset-password{
+  position: relative;
+  .form-wrap{
+    .reset{
+      h2{
+        margin-bottom: 8px;
+      }
+    }
+  }
+}
+</style>
