20e77f628b6ead741d88e7da1d682bfdcdbbe381
Nam <njh3421@naver.com>
2021-10-21 22:43:12 +0900

    Add profile menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,40 +1,76 @@
 <template>
-    <header>
-        <nav class="container">
-            <div class="branding">
-                <router-link class="header" :to="{name: 'Home'}">FireBlogs</router-link>
-            </div>
-            <div class="nav-links">
-                <ul v-show="!mobile">
-                    <router-link class="link" :to="{name:'Home'}">Home</router-link>
-                    <router-link class="link" :to="{name:'Blogs'}">Blogs</router-link>
-                    <router-link class="link" :to="{name:'Blogs'}">Create Post</router-link>
-                    <router-link class="link" :to="{name:'Login'}">Login/Register</router-link>
-                </ul>
-            </div>
-        </nav>
-        <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile"/>
-        <!-- 아래 transition은 모바일로 볼때 즉 화면이 줄었을때 메뉴를 나타낸다 -->
-        <transition name="mobile-nav">
-        <ul class="mobile-nav" v-show="mobileNav">
+  <header>
+   <nav class="container">
+    <div class="branding">
+           <router-link class="header" :to="{name: 'Home'}">FireBlogs</router-link>
+    </div>
+    <div class="nav-links">
+        <ul v-show="!mobile">
             <router-link class="link" :to="{name:'Home'}">Home</router-link>
             <router-link class="link" :to="{name:'Blogs'}">Blogs</router-link>
-            <router-link class="link" to="#">Create Post</router-link>
-            <router-link class="link" to="Login">Login/Register</router-link>
+            <router-link class="link" :to="{name:'Blogs'}">Create Post</router-link>
+            <router-link class="link" :to="{name:'Login'}">Login/Register</router-link>
         </ul>
-        </transition>
-    </header>
+    <div @click="toggleProfileMenu" class="profile" ref="profile">
+        <span>{{this.$store.state.profileInitials}}</span>
+     <div v-show="profileMenu" class="profile-menu">
+        <div class="info">
+            <p class="initials">{{this.$store.state.profileInitials}}</p>
+        <div class="right">
+            <p>{{this.$store.state.profileFirstName}} {{this.$store.state.profilelastName}}</p>
+            <p>{{this.$store.state.profileUsername}}</p>
+            <p>{{this.$store.state.profileEmail}}</p>
+          </div>
+         </div>
+         <div class="options">
+             <div class="option">
+                 <router-link class="option" to="#">
+                     <userIcon class="icon"/>
+                     <p>profile</p>
+                 </router-link>
+             </div>
+             <div class="option">
+                 <router-link class="option" to="#">
+                     <adminIcon class="icon"/>
+                     <p>Admin</p>
+                 </router-link>
+             </div>
+             <div @click="signOut" class="option">
+                     <signOutIcon class="icon"/>
+                     <p>Sign out</p>
+             </div>
+         </div>
+      </div>
+     </div>
+    </div>
+   </nav>
+   <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile"/>
+   <!-- 아래 transition은 모바일로 볼때 즉 화면이 줄었을때 메뉴를 나타낸다 -->
+   <transition name="mobile-nav">
+   <ul class="mobile-nav" v-show="mobileNav">
+       <router-link class="link" :to="{name:'Home'}">Home</router-link>
+       <router-link class="link" :to="{name:'Blogs'}">Blogs</router-link>
+       <router-link class="link" to="#">Create Post</router-link>
+       <router-link class="link" to="Login">Login/Register</router-link>
+   </ul>
+   </transition>
+  </header>
 </template>
 <script>
 import menuIcon from '../assets/Icons/bars-regular.svg';
-
+import userIcon from '../assets/Icons/user-alt-light.svg';
+import adminIcon from '../assets/Icons/user-crown-light.svg';
+import signOutIcon from '../assets/Icons/sign-out-alt-regular.svg';
+import firebase from "firebase/app";
+import "firebase/auth";
 export default {
     name: 'navigation',
     components: {
-        menuIcon,
+        menuIcon,userIcon,adminIcon,signOutIcon
     },
     data() {
         return {
+            profileMenu:null,
             //모바일 동작시 모바일 요소만 보여지게 하기위해 세팅
             mobile:null,
             mobileNav:null,
@@ -60,6 +96,15 @@ export default {
         //모바일 네비게이션 토글
         toggleMobileNav() {
             this.mobileNav=!this.mobileNav;
+        },
+        toggleProfileMenu(e){
+            if(e.target===this.$refs.profile){
+                this.profileMenu= !this.profileMenu;
+            }
+        },
+        signOut(){
+            firebase.auth().signOut();
+            window.location.reload();
         }
     }
 };
@@ -112,6 +157,87 @@ nav{
             .link:last-child{
                 margin-right: 0;
             }
+        }
+        
+        .profile{
+         position: relative;
+         cursor: pointer;
+         display: flex;
+         align-items: center;
+         justify-content: center;
+         width: 40px;
+         height: 40px;
+         border-radius: 50%;
+         color: #fff;
+         background-color: #303030;
+         
+         span{
+             pointer-events: none;
+         }
+
+         .profile-menu{
+             position: absolute;
+             top: 60px;
+             right: 0;
+             width: 250px;
+             background-color: #303030;
+             box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.06),0 2px 4px -1px rgba(0, 0, 0, 0.06);
+             
+             .info{
+                 display: flex;
+                 align-items: center;
+                 padding: 15px;
+                 border-bottom: 1px solid #fff;
+             }
+             .options{
+                 padding: 15px;
+                 .option{
+                     text-decoration: none;
+                     color: #fff;
+                     display: flex;
+                     align-items: center;
+                     margin-bottom: 12px;
+
+                     .icon {
+                         width: 18px;
+                         height: auto;
+                     }
+
+                     p{
+                         font-size: 14px;
+                         margin-left: 12px;
+                     }
+
+                     &:last-child{
+                         margin-bottom: 0px;
+                     }
+                 }
+             }
+
+             .initials{
+                 position: initial;
+                 width: 40px;
+                 height: 40px;
+                 background-color: #fff;
+                 color: #303030;
+                 display: flex;
+                 align-items: center;
+                 justify-content: center;
+                 border-radius: 50%;
+             }
+             
+             .right{
+                 flex: 1;
+                 margin-left: 24px;
+                 p:nth-child(1){
+                     font-size: 14px;
+                 }
+                 p:nth-child(2),
+                 p:nth-child(3){
+                     font-size: 12px;
+                 }
+             }
+         }
         }
     }
 }
