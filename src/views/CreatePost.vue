8e08367ddd84db93f8768db7a2a5e1b7975bc660
Nam <njh3421@naver.com>
2021-11-01 17:29:24 +0900

    ended CreatePost

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -15,8 +15,7 @@
         </div>
       </div>
       <div class="editor">
-        <!-- @image-added="imageHandler" -->
-        <vue-editor :editorOptions="editorSettings" v-model="blogHTML" useCustomImageHandler />  
+        <vue-editor :editorOptions="editorSettings" v-model="blogHTML" useCustomImageHandler @image-added="imageHandler"/>  
       </div>
       <div class="blog-actions">
         <button>Publish Blog</button>
@@ -27,7 +26,9 @@
 </template>
 
 <script>
-import BlogCoverPreview from "../components/BlogCoverPreview.vue"
+import BlogCoverPreview from "../components/BlogCoverPreview.vue";
+import firebase from "firebase/app";
+import "firebase/storage"
 import Quill from "quill";
 window.Quill=Quill;
 const ImageResize =require("quill-image-resize-module").default;
@@ -59,7 +60,25 @@ export default {
     },
     openPreview(){
       this.$store.commit("openPhotoPreview")
-    }
+    },
+    imageHandler(file,Editor,cursorLocation,resetUploader){
+      const storageRef=firebase.storage().ref();
+      const docRef=storageRef.child(`documents/blogPostPhotos/${file.name}`);
+      docRef.put(file).on(
+        "state_changed",
+      (snapshot) =>{
+        console.log(snapshot);
+      }, 
+      (err)=>{
+        console.log(err);
+      }, 
+      async()=>{
+        const downloadURL=await docRef.getDownloadURL();
+        Editor.insertEmbed(cursorLocation,"image",downloadURL);
+        resetUploader();
+        }
+      );
+    },
   },
   computed:{
     profileId(){
